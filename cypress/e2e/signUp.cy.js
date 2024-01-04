21b151dbbcd7fb30bf73f8a8ed04d8268533f387
darekkieuki <d.wojtasiak89@gmail.com>
2024-01-04 16:11:46 +0100

    Update signUp.cy.js

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -40,12 +40,38 @@ import {
 describe("Creates new account", () => {
   it("sucks cock", () => {
     cy.visit("/login");
-    cy.get(signupName).type(randomUserName);
-    cy.get(signupMail).type(randomUserEmail);
-    cy.get(signUpButton).click();
-    cy.get(nameInput).invoke("attr", "value").should("contain", savedUsername);
-    cy.get(emailInput).invoke("attr", "value").should("contain", savedUserEmail);
-    cy.get(signUpPassword).type(randomPassword);
+    signUp(randomUserName, randomUserEmail)
+    register(savedUsername, savedUserEmail, randomPassword)
+    // cy.get(nameInput).invoke("attr", "value").should("contain", savedUsername);
+    // cy.get(emailInput).invoke("attr", "value").should("contain", savedUserEmail);
+    // cy.get(signUpPassword).type(randomPassword);
+    // cy.get(daySelector).select(randomDay);
+    // cy.get(monthSelector).select(randomMonth);
+    // cy.get(yearSelector).select(randomYear);
+    // cy.get(firstNameInput).type(randomFirstName);
+    // cy.get(lastNameInput).type(randomLastName);
+    // cy.get(addressInput).type(randomAddress);
+    // cy.get(countrySelector).select(randomCountry);
+    // cy.get(stateInput).type(randomState);
+    // cy.get(cityInput).type(randomCity);
+    // cy.get(zipcodeInput).type(randomZipcode);
+    // cy.get(mobileInput).type(randomPhoneNumber);
+    // cy.get(registerButton).click();
+  });
+});
+
+
+
+function signUp(name, email){
+  cy.get(signupName).type(name);
+  cy.get(signupMail).type(email);
+  cy.get(signUpButton).click();
+}
+
+function register(name, email, password){
+  cy.get(nameInput).invoke("attr", "value").should("contain", name);
+    cy.get(emailInput).invoke("attr", "value").should("contain", email);
+    cy.get(signUpPassword).type(password);
     cy.get(daySelector).select(randomDay);
     cy.get(monthSelector).select(randomMonth);
     cy.get(yearSelector).select(randomYear);
@@ -58,5 +84,4 @@ describe("Creates new account", () => {
     cy.get(zipcodeInput).type(randomZipcode);
     cy.get(mobileInput).type(randomPhoneNumber);
     cy.get(registerButton).click();
-  });
-});
+}
